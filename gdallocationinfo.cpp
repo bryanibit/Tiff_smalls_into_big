5d09341d40cef950ca91a1346d3e1b7375697e50
bryan <bryanibit@gmail.com>
2019-06-30 08:16:51 +0800

    attach img success

--- gdallocationinfo.cpp
+++ gdallocationinfo.cpp
@@ -10,9 +10,6 @@
 #include "transform.h"
 #include <iomanip>
 #include "opencv2/opencv.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/core/core.hpp"
-#include <tuple>
 
 using namespace std;
 using namespace cv;
@@ -47,7 +44,7 @@ vector<PointUTM> acquireRoadPoint(string file_name){
         double UTMNorthing = 0;
         double UTMEasting = 0;
         int UTMZone;
-        LatLonToUTMXY(std::stod(line.at(2)), std::stod(line.at(1)), UTMZone, UTMEasting, UTMNorthing);
+        XY_LB::Mercator::LB2XY(std::stod(line.at(1)), std::stod(line.at(2)), UTMEasting, UTMNorthing);
         std::cout << std::setprecision(12) << UTMEasting << ", " << std::setprecision(12) << UTMNorthing << std::endl;
         res.emplace_back(UTMEasting, UTMNorthing);
     }
@@ -90,31 +87,36 @@ int main()
     cv::Mat smallImg = cv::imread(odm_img, 1);
     cout << "img size: (" << smallImg.size().width << ", " << smallImg.size().height << ")" << std::endl;
 
-    // combine two image to one
+    /// combine two image to one
     const char *ground_img = "/home/ugv-yu/bryan/test/geotiff/alashanNorth.tif";
     auto topLeftPoint_ground = getTiffTopLeft(ground_img);
     auto groudImg = cv::imread(ground_img, 1);
     cout << "ground img size: " << groudImg.size().width << ", " << groudImg.size().height << std::endl;
 
-    // smallImg: x, y range -> bigImg pixel range
+    /// read road points
+    string roadFile = "/home/ugv-yu/bryan/test/geotiff/paowei_way.txt";
+    auto roadPoints = acquireRoadPoint(roadFile);
+
+    /// read road network
+    string roadNet = "/home/ugv-yu/bryan/test/geotiff/alashanroad.txt";
+    auto roadNetPoints = acquireRoadPoint(roadNet);
+
+    /// read first day hole points
+    string roadFirstDay = "/home/ugv-yu/bryan/test/geotiff/alashan.txt";
+    auto firstDayPoints = acquireRoadPoint(roadFirstDay);
+
+    /// smallImg: x, y range -> bigImg pixel range
     double bigResolution = topLeftPoint_ground.at(1).x;
     int width_in_bigImg = static_cast<int>(smallImg.size().width * topLeftPoint_odm.at(1).x / topLeftPoint_ground.at(1).x);
     int height_in_bigImg = static_cast<int>(smallImg.size().height * topLeftPoint_odm.at(1).y / topLeftPoint_ground.at(1).y);
     cout << "small img within big in pixel: " << width_in_bigImg << ", " << height_in_bigImg << std::endl;
-    // smallImg resize to above range then mask
+    /// smallImg resize to above range then mask
     cv::Mat smallImg_in_bigImg;
     cv::resize(smallImg,smallImg_in_bigImg, cv::Size(width_in_bigImg, height_in_bigImg), 0 ,0);
     cv::imshow("img", smallImg_in_bigImg);
     cv::waitKey(0);
 
-//    auto topRightBig = PointUTM(11754466.9511462640, 4723788.790894158);
-//    auto pixel_x = (topLeftPoint_odm.at(0).x- topRightBig.x) / bigResolution;
-//    auto pixel_y = ((topRightBig.y + groudImg.size().height * bigResolution)- topLeftPoint_odm.at(0).y) / bigResolution;
     int zone = 48;
-//    double x_=0;  double y_ = 0; double test_lat = 39.03767;double test_lon = 105.60;
-//    LatLonToUTMXY(test_lat,test_lon,zone,x_, y_);
-//    cout << "zone: " << zone << std::endl;
-//    cout << "test_lon_lat: " << test_lat << ", " << test_lon << std::endl;
     double lat_topleft = 0;
     double lon_topleft = 0;
     UTMXYToLatLon(topLeftPoint_odm.at(0).x, topLeftPoint_odm.at(0).y, zone, false,lat_topleft, lon_topleft);
@@ -134,7 +136,28 @@ int main()
             smallImg_in_bigImg.size().width, smallImg_in_bigImg.size().height);
 
     smallImg_in_bigImg.copyTo(groudImg(rec));
-    cv::imwrite("res.jpg", groudImg);
+
+    for(const auto & point: roadPoints){
+        auto pi_x = (point.x - topLeftPoint_ground.at(0).x) / topLeftPoint_ground.at(1).x;
+        auto pi_y = (point.y - topLeftPoint_ground.at(0).y) / topLeftPoint_ground.at(1).y;
+        cv::circle(groudImg,cv::Point(pi_x, pi_y), 1, cv::Scalar(0,25,255), -1);
+    }
+
+    for(const auto & point: roadNetPoints){
+        auto pi_x = (point.x - topLeftPoint_ground.at(0).x) / topLeftPoint_ground.at(1).x;
+        auto pi_y = (point.y - topLeftPoint_ground.at(0).y) / topLeftPoint_ground.at(1).y;
+        cv::circle(groudImg,cv::Point(pi_x, pi_y), 1, cv::Scalar(255,25,0), -1);
+    }
+
+    for(const auto & point: firstDayPoints){
+        auto pi_x = (point.x - topLeftPoint_ground.at(0).x) / topLeftPoint_ground.at(1).x;
+        auto pi_y = (point.y - topLeftPoint_ground.at(0).y) / topLeftPoint_ground.at(1).y;
+        cv::circle(groudImg,cv::Point(pi_x, pi_y), 1, cv::Scalar(0,255,0), -1);
+    }
+//    cv::resize(groudImg, groudImg, cv::Size(0,0), 0.1,0.1);
+//    cv::imshow("res", groudImg);
+    cv::imwrite("all.jpg", groudImg);
+//    cv::imshow("smallImg", groudImg(rec));
     cv::waitKey(100);
     return 0;
 }
